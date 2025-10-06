2e12b3200c867a9905666d20d14b45d2f9045841
Davi Severino Oliveira de Souza <daviseverinoinf2@gmail.com>
2025-10-06 14:06:14 -0300

    .

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,3 +1,11 @@
 <header>
-    <h1>DAVIIIIIIIIIIIIIII SEVERINO</h1>
+    <h1>CRUZEIRO</h1>
+    <img src="images/maxresdefault.jpg" alt="">
 </header>
+<style>
+    h1{
+        font-family: "Roboto Thin", sans-serif;
+        color: chocolate;
+        
+    }
+</style>
