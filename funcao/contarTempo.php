b3bd5c309444b2ea257f782e5062ebdf4d85d771
SamuelaAraujo <daviseverinoinf2@gmail.com>
2025-10-27 15:22:45 -0300

    Update contarTempo.php

--- funcao/contarTempo.php
+++ funcao/contarTempo.php
@@ -4,22 +4,23 @@ function contarTempo(string $data):string
     $agora = strtotime(date(format: 'Y/m/d h:i:s'));
     $tempo = strtotime($data);
 
+
+
     echo ($agora);
     echo "<hr>";
-    echo ($tempo);
-
+    echo ("Data digitada" . $tempo);
     $diferenca = $agora - $tempo;
+    echo ($tempo);
     echo "<hr>";
     echo ($diferenca);
 
     $segundos = $diferenca;
-    $minutos = round($diferenca / 60);
-    $segundos = round($diferenca / 3600);
-    $horas = round($diferenca / 86400);
-    $dias = round($diferenca / 86400);
-    $semanas = round($diferenca / 604800);
-    $meses = round($diferenca / 2600640);
-    $anos = round($diferenca / 31207680);
+    $minutos = round($diferenca / 60); // um minuto tem 60 segundos.
+    $horas = round($diferenca / 3600); // uma hora tem 60 minutos.
+    $dias = round($diferenca / 86400); // um dia tem 24 horas.
+    $semanas = round($diferenca / 604800); // uma semana tem 7 dias
+    $meses = round($diferenca / 2419200); // um mês tem 4 semanas
+    $anos = round($diferenca / 29030400); // um ano tem 12 meses
 
     return true;
 
