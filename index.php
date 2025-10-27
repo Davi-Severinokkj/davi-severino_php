4eaac8a20826d489fb04b88bd96e907398391a22
Davi-Severinokkj <daviseverinoinf2@gmail.com>
2025-10-27 14:11:31 -0300

    .

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
 
+date_default_timezone_set('America/Sao_Paulo');
 
+function saudacao():string
+{
+    $hora = date('H');
+    if ($hora >= 0 && $hora < 5) {
+        $saudacao = 'Boa madrugada!';
+    } else if ($hora >= 6 && $hora < 10) {
+        $saudacao = 'Bom dia!';
+    } else if ($hora >= 11 && $hora < 17) {
+        $saudacao = 'Boa tarde!';
+    } else if ($hora >= 18 && $hora < 23) {
+        $saudacao = 'Boa noite!';
+    }
 
-
+    return $saudacao;
+}
 ?>
---
+++ index_saudacao.php
@@ -0,0 +1,6 @@
+<?php
+
+    include 'funcao/saudacao.php';
+    echo '<hr>';
+    echo '<h3>FUNÇÃO SAUDAÇÃO</h3>';
+    echo saudacao();
