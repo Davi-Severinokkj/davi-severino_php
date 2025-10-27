9aef5ecbb6f3984f5ce6c3e462da74c7c0a0aca5
SamuelaAraujo <sammueel32@gmail.com>
2025-10-27 15:17:22 -0300

    Update contarTempo.php

--- funcao/contarTempo.php
+++ funcao/contarTempo.php
@@ -8,6 +8,19 @@ function contarTempo(string $data):string
     echo "<hr>";
     echo ($tempo);
 
+    $diferenca = $agora - $tempo;
+    echo "<hr>";
+    echo ($diferenca);
+
+    $segundos = $diferenca;
+    $minutos = round($diferenca / 60);
+    $segundos = round($diferenca / 3600);
+    $horas = round($diferenca / 86400);
+    $dias = round($diferenca / 86400);
+    $semanas = round($diferenca / 604800);
+    $meses = round($diferenca / 2600640);
+    $anos = round($diferenca / 31207680);
+
     return true;
 
 }
