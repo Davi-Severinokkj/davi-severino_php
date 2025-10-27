99d83923b9f6802190ff22e532e922955ff1a0fb
Davi-Severinokkj <daviseverinoinf2@gmail.com>
2025-10-27 13:29:46 -0300

    .

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-    include "assets/includes/doctype.php";
-?>
-    <title>Página principal</title>
-    <link rel="stylesheet" href="assets/includes/style/style.css">
-</head>
-<body>
-<?php
-    include "assets/includes/header.php";
-    include "assets/includes/navigationMenu.php";
-    include "forms/cadastro.php";
-?>
 
-</body>
+
+
+
+
+?>
